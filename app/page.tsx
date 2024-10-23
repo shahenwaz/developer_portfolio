85aee803dd25c71c19f5687b4437aaf80035536d
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-10-23 23:39:51 +0100

    Component added - Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     // Justify-center (center's horizontally), items-center (center's vertically)
     <main className="relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden bg-black-100 sm:px-10">
       <div className="w-full max-w-7xl">
-        <h1>Hi, this is my Portfolio.</h1>
+        <Hero />
       </div>
     </main>
   );
