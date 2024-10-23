b8753bd5e5518491d6e86e5b5dff89944a7169cd
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-10-24 00:15:06 +0100

    Component added - Spotlight from Aceternity UI

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div></div>
+      <div>
+        <Spotlight
+          className="h-screen -top-40 -left-10 md:-left-32 md:-top-20"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="h-[80vh] w-[50vw] top-28 left-80" fill="blue" />
+      </div>
     </div>
   );
 };
